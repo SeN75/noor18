4fb8b70b9603179e23012d2c936ef87fdf304217
saleh alsaggaf <saa111954@hotmail.com>
2021-05-20 07:28:38 +0300

    create footer - content is messing/

---
+++ src/app/view/components/footer/footer.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { ICON } from './../../../_common/globle';
+
+@Component({
+  selector: 'footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent implements OnInit {
+  icon = ICON;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
