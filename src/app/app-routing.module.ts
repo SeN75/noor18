a0cf025e77c6706bc1b2460447fa0592f3dc1415
saleh alsaggaf <saa111954@hotmail.com>
2021-05-20 07:29:27 +0300

    add navbar and footer in base component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BaseComponent } from './view/components/base/base.component';
+import { LandingpageComponent } from './view/landingpage/landingpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { component: LandingpageComponent, path: '' },
+  { component: BaseComponent, path: 'base' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
